1cd5c978046c2fbc66c6068fdcfa44ddff9786c4
root <root@ivi.trustservers.gr>
2017-03-05 12:34:25 +0200

    0.96 only readme changes

--- slug-length-extender.php
+++ slug-length-extender.php
@@ -12,7 +12,7 @@ Useful for permalinks using non latin characters in URLs. Long permalinks will n
 
 Author: Giannis Economou
 
-Version: 0.95
+Version: 0.96
 
 Author URI: http://www.antithesis.gr
 
